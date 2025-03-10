37d8a753d0228ba662567ecb300b9ee766fa1eb7
tabatanovillo239 <tabatanovillo239@gmail.com>
2025-03-10 19:27:32 +0000

    second-commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tgallery.app',
+  appName: 'com.tgallery.app',
+  webDir: 'dist/myapp/browser'
+};
+
+export default config;
